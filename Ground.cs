46f4a2de3db17e28347c620b37c8c6d3e87a9071
davMac606 <cl201238@g.unicamp.br>
2022-08-15 23:48:32 -0300

    Adição de classes Ground, Air e Parasite

---
+++ Ground.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoBio
+{
+    public class Ground
+    {
+        public List<Ground> _lstGround = new List<Ground>();
+
+        public int Num { get; set; }
+        public string Nome { get; set; }
+        public string Classe { get; set; }
+
+        public Ground(int num, string nome, string classe)
+        {
+            Num = num;
+            Nome = nome;
+            Classe = classe;
+        }
+    
+        public Ground()
+        {
+        }
+
+        public void AddToList(int num, string nome, string classe)
+        {
+            _lstGround.Add(new Ground(num, nome, classe));
+        }
+
+        public List<Ground> ToList()
+        {
+            return _lstGround;
+        }
+
+        public List<Ground> GeraAnimais()
+        {
+            AddToList(1, "Niphates alba Van Soest", "Porifero");
+            AddToList(2, "Polvo Dumbo", "Molusco");
+            return _lstGround;
+        }
+    }
+}
---
+++ Parasite.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoBio
+{
+    public class Parasite
+    {
+        public List<Parasite> _lstParasite = new List<Parasite>();
+
+        public int Num { get; set; }
+        public string Nome { get; set; }
+        public string Classe { get; set; }
+
+        public Parasite(int num, string nome, string classe)
+        {
+            Num = num;
+            Nome = nome;
+            Classe = classe;
+        }
+
+        public Parasite()
+        {
+        }
+
+        public void AddToList(int num, string nome, string classe)
+        {
+            _lstParasite.Add(new Parasite(num, nome, classe));
+        }
+
+        public List<Parasite> ToList()
+        {
+            return _lstParasite;
+        }
+
+        public List<Parasite> GeraAnimais()
+        {
+            AddToList(1, "Niphates alba Van Soest", "Porifero");
+            AddToList(2, "Polvo Dumbo", "Molusco");
+            return _lstParasite;
+        }
+    }
+}
+}
