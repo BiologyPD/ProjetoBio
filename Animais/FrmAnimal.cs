8d99e390e19bd8a96bee42ece66ecf4caf15721c
Meida <99834864+AmeMeida@users.noreply.github.com>
2022-09-10 18:02:27 -0300

    FrmAnimal alterado (ESTÁ CRASHANDO)
    Agora ele tem um método pra colocar os values do enum dentro da combobox. Não que esteja funcionando, mas tbm n sei pq.

--- Animais/FrmAnimal.cs
+++ Animais/FrmAnimal.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -14,13 +14,26 @@ namespace ProjetoBio.Animais
     {
         public FrmAnimal()
         {
+            MessageBox.Show(Enum.GetValuesOf<Filo>().ToString());
             InitializeComponent();
-            cbFilo.DataSource(Filo);
+            //CarregarCB<Respiracao>(cbRespiracao);
+            CarregarCB<Filo>(cbFilo);
+        }
+
+        private void CarregarCB<T>(ComboBox cb) where T : Enum
+        {
+            cb.DisplayMember = "Key";
+            cb.ValueMember = "Value";
+            cb.DataSource = KeyPair<T>.GetKeyPairs();
+            // cb.DataSource = Enum.GetValuesOf<T>().Select(e => new { Key = e.Text, Value = e }).ToList();
+            // cb.DataSource = KeyPair<T>.GetKeyPairs();
+            // cb.DataSource = KeyPair<Filo>.GetKeyPairs();
+            // cb.DataSource = KeyPair<T>.GetKeyPairs(Enum.GetValuesOf<T>());
         }
 
         private void cbFilo_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            MessageBox.Show(((Enum) cbFilo.SelectedValue).Text);
         }
     }
 }
